9049e857e2964fc0a4b96a508e851582fccc5916
usama-mr20 <usamamustafa89@gmail.com>
2022-03-18 01:17:51 +0500

    structured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import Home from "./home";
+import "./App.css";
+import Navbar from "./components/NavBar";
+import Home from "./layout-pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
